36f5c83b029080374edaad92dbea80510221d1b8
munir79 <jakirhossainmunir79@gmail.com>
2024-02-27 21:01:09 +0600

    sort

--- Operation.cpp
+++ Operation.cpp
@@ -3,11 +3,26 @@ using namespace std;
 int main(){
 
     //1. Remove 
-    list <int>myList={1 ,2,3,4,5,10,10,10};
+    list <int>myList={100 ,2,3,4,5,10,10,10};
     myList.remove(10);
     for(int val :myList){
         cout<<val<<" ";
     }
     cout<<endl;
+
+    //2 sort ascending order sort
+   myList.sort();
+   for(int val:myList){
+    cout<<val<<" ";
+   }
+   cout<<endl;
+
+   //3 decing order sort
+   myList.sort(greater<int>());
+   for(int val:myList){
+    cout<<val<<" ";
+   }
+   cout<<endl;
+
 
 }
