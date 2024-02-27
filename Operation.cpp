f39e721c1cc2fe7e9dcc7261b6037a1cafe8a1ae
munir79 <jakirhossainmunir79@gmail.com>
2024-02-27 22:03:27 +0600

    reverse

--- Operation.cpp
+++ Operation.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 
-    //1. Remove 
+//     //1. Remove 
     list <int>myList={100 ,2,3,4,5,10,10,10};
     myList.remove(10);
     for(int val :myList){
@@ -10,21 +10,21 @@ int main(){
     }
     cout<<endl;
 
-    //2 sort ascending order sort
+//     //2 sort ascending order sort
    myList.sort();
    for(int val:myList){
     cout<<val<<" ";
    }
    cout<<endl;
 
-   //3 decing order sort
+//    //3 decing order sort
    myList.sort(greater<int>());
    for(int val:myList){
     cout<<val<<" ";
    }
    cout<<endl;
 
-   //4. remove Duplicate  (Note : firstly need sort array)
+//    //4. remove Duplicate  (Note : firstly need sort array)
    myList.sort();
    myList.unique();
    for(int val:myList){
@@ -32,7 +32,14 @@ int main(){
    }
    cout<<endl;
   
-
+//5.Reverse 
+myList.reverse();
+//  myList.sort();
+//    myList.unique();
+   for(int val:myList){
+    cout<<val<<" ";
+   }
+   cout<<endl;
 
 
 
